e145efddf2a66698f0657635b90166176ef1db6b
Ivo Donchev <i.donchev@hacksoft.io>
2020-11-12 10:33:45 +0200

    Remove consumer

--- daphne_3_issue_demo/daphne_3_issue_demo/routing.py
+++ daphne_3_issue_demo/daphne_3_issue_demo/routing.py
@@ -2,13 +2,6 @@ from django.conf.urls import url
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from .consumers import MyConsumer
-
-
 application = ProtocolTypeRouter({
-    'websocket': URLRouter(
-        [
-            url(r'^ws/$', MyConsumer.as_asgi()),
-        ]
-    )
+    'websocket': URLRouter([])
 })
